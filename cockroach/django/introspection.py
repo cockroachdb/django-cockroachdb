b9d67a75f794fa14191dd328d1f45440479ac37c
Tim Graham <timograham@gmail.com>
2019-10-07 19:43:42 -0400

    Fix typo in cockroach/django/introspection.py

--- cockroach/django/introspection.py
+++ cockroach/django/introspection.py
@@ -4,11 +4,10 @@ from django.db.backends.postgresql.introspection import  \
 
 
 class DatabaseIntrospection(PostgresDatabaseIntrospection):
-    # 1185 is TIMESTAMPZ so maping it back to DateTimeField
     data_types_reverse = dict(PostgresDatabaseIntrospection.data_types_reverse)
 
-    data_types_reverse[1184] = 'DateTimeField'
-    
+    data_types_reverse[1184] = 'DateTimeField'  # TIMESTAMPZ
+
     def get_table_list(self, cursor):
         cursor.execute("SHOW TABLES")
         # The second TableInfo field is 't' for table or 'v' for view.
