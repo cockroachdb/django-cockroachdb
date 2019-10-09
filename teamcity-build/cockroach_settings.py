d8243861b7b6e811aee10228a0d89ed16af50160
Rohan Yadav <rohany@cockroachlabs.com>
2019-10-09 14:53:52 -0400

    ci: Add teamcity continuous integration
    This PR adds continuous integration for running parts of the django test
    suite on teamcity. To adjust the django test apps that are executed in
    the tests, edit the teamcity-build/runtests.py script.

---
+++ teamcity-build/cockroach_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'cockroach.django',
+        'NAME' : 'django_tests',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST': 'localhost',
+        'PORT' : 26257,
+    },
+}
+SECRET_KEY = 'django_tests_secret_key'
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
