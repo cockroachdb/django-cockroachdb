3be02c9a1a528b89a767cd5e484a2c4f20704dc8
Tim Graham <timograham@gmail.com>
2019-10-14 10:08:21 -0400

    Disable can_introspect_materialized_views feature

--- cockroach/django/features.py
+++ cockroach/django/features.py
@@ -62,3 +62,7 @@ class DatabaseFeatures(PostgresDatabaseFeatures):
 
     # Not implemented by cockroachdb: https://github.com/cockroachdb/cockroach/issues/41645
     supports_regex_backreferencing = False
+
+    # Introspection may work but 'CREATE MATERIALIZED VIEW' (required for the
+    # test) isn't implemented: https://github.com/cockroachdb/cockroach/issues/41649
+    can_introspect_materialized_views = False
