f58437d7e097d03c3c842c94b723776b27e717a2
Tim Graham <timograham@gmail.com>
2019-11-15 11:17:42 -0500

    clean up docstrings that should be inline comments

--- django_cockroachdb/operations.py
+++ django_cockroachdb/operations.py
@@ -7,19 +7,16 @@ from pytz import timezone
 
 
 class DatabaseOperations(PostgresDatabaseOperations):
-    """
-    Cockroach currently doesn't allow for deferrable constraints. Feature is
-    tracked here: https://github.com/cockroachdb/cockroach/issues/31632
-    """
     def deferrable_sql(self):
+        # Deferrable constraints aren't supported:
+        # https://github.com/cockroachdb/cockroach/issues/31632
         return ''
 
-    """
-    If a datetime doesn't have a tzinfo then psycopg2 will cast
-    the datetime to a TIMESTAMP, which will cause a type error in Cockroach. If
-    we specify a tzinfo then psycopg2 will cast it to a TIMESTAMPTZ
-    """
     def adapt_datetimefield_value(self, value):
+        """
+        Add a timezone to datetimes so that psycopg2 will cast it to
+        TIMESTAMPTZ (as cockroach expects) rather than TIMESTAMP.
+        """
         # getattr() guards against F() objects which don't have tzinfo.
         if value and getattr(value, 'tzinfo', '') is None and self.connection.timezone_name is not None:
             connection_timezone = timezone(self.connection.timezone_name)
@@ -34,7 +31,7 @@ class DatabaseOperations(PostgresDatabaseOperations):
         return value
 
     def sequence_reset_by_name_sql(self, style, sequences):
-        # Not implemented by cockroachdb: https://github.com/cockroachdb/cockroach/issues/20956
+        # Not implemented: https://github.com/cockroachdb/cockroach/issues/20956
         return []
 
     def sequence_reset_sql(self, style, model_list):
