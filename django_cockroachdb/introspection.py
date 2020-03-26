dc44068cb7f469acaa21b8cb8e0299d56ae88f02
Oliver Tan <otan@cockroachlabs.com>
2020-03-26 10:56:46 -0700

    django_cockroachdb: use table_name from SHOW TABLES for get_table_list
    In 20.2, we're investigating putting more columns into SHOW TABLES. As
    such, this query will need to update in a backwards compatible way. The
    query has thus been modified to support both old and new versions.

--- django_cockroachdb/introspection.py
+++ django_cockroachdb/introspection.py
@@ -7,10 +7,10 @@ from django.db.models import Index
 
 class DatabaseIntrospection(PostgresDatabaseIntrospection):
     data_types_reverse = dict(PostgresDatabaseIntrospection.data_types_reverse)
-    data_types_reverse[1184] = 'DateTimeField'  # TIMESTAMPZ
+    data_types_reverse[1184] = 'DateTimeField'  # TIMESTAMPTZ
 
     def get_table_list(self, cursor):
-        cursor.execute("SHOW TABLES")
+        cursor.execute("SELECT table_name FROM [SHOW TABLES]")
         # The second TableInfo field is 't' for table or 'v' for view.
         return [TableInfo(row[0], 't') for row in cursor.fetchall()]
 
