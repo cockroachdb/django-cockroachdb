36e545112feb7e5255c79283b3a867b3e04656f7
Tim Graham <timograham@gmail.com>
2020-07-28 19:37:14 -0400

    bump version to 3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## 3.1 - Unreleased
+## 3.1 - 2020-08-05
 
 Initial release for Django 3.1.x and CockroachDB 20.1.
--- django_cockroachdb/__init__.py
+++ django_cockroachdb/__init__.py
@@ -2,7 +2,7 @@ from .functions import register_functions
 from .lookups import patch_lookups
 from .utils import check_django_compatability
 
-__version__ = '3.1a0'
+__version__ = '3.1'
 
 check_django_compatability()
 patch_lookups()
