471d0212ab256dd5a7fecd548f74da3bca0c3e85
Tim Graham <timograham@gmail.com>
2020-11-14 20:09:52 -0500

    run Django compatability check before importing other code
    fixes #191

--- django_cockroachdb/__init__.py
+++ django_cockroachdb/__init__.py
@@ -1,7 +1,11 @@
-from .functions import register_functions
-from .utils import check_django_compatability
-
 __version__ = '2.2.2'
 
+# Check Django compatibility before other imports which may fail if the
+# wrong version of Django is installed.
+from .utils import check_django_compatability
+
 check_django_compatability()
+
+from .functions import register_functions  # noqa
+
 register_functions()
