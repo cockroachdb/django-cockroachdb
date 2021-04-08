cba73c35383c4f2d4f5ab4269c34fca0e831389b
Tim Graham <timograham@gmail.com>
2021-04-08 10:21:32 -0400

    bump version to 3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## 3.2 - Unreleased
+## 3.2 - 2021-04-08
 
 Initial release for Django 3.2.x and CockroachDB 20.2.
--- django_cockroachdb/__init__.py
+++ django_cockroachdb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.2a0'
+__version__ = '3.2'
 
 # Check Django compatibility before other imports which may fail if the
 # wrong version of Django is installed.
