931914a427859bfaca7e0dc0de8d996e47a016f3
Tim Graham <timograham@gmail.com>
2021-07-14 17:51:49 -0400

    remove DatabaseIntrospection.get_table_list()
    CockroachDB supports the query that the PostgreSQL backend uses.

--- django_cockroachdb/introspection.py
+++ django_cockroachdb/introspection.py
@@ -1,4 +1,3 @@
-from django.db.backends.base.introspection import TableInfo
 from django.db.backends.postgresql.introspection import (
     DatabaseIntrospection as PostgresDatabaseIntrospection,
 )
@@ -9,11 +8,6 @@ class DatabaseIntrospection(PostgresDatabaseIntrospection):
     data_types_reverse = dict(PostgresDatabaseIntrospection.data_types_reverse)
     data_types_reverse[1184] = 'DateTimeField'  # TIMESTAMPTZ
     index_default_access_method = 'prefix'
-
-    def get_table_list(self, cursor):
-        cursor.execute("SELECT table_name, type FROM [SHOW TABLES]")
-        # The second TableInfo field is 't' for table or 'v' for view.
-        return [TableInfo(row[0], 't' if row[1] == 'table' else 'v') for row in cursor.fetchall()]
 
     def get_constraints(self, cursor, table_name):
         """
