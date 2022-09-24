31b32f2943cca293ba38c40f8b4b8670027a2018
Tim Graham <timograham@gmail.com>
2022-09-24 17:22:06 -0400

    ci: update to cockroach-v22.2.1, v22.1.12, and v21.2.17

--- teamcity-build/build-teamcity-22.1.sh
+++ teamcity-build/build-teamcity-22.1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-./teamcity-build/build-teamcity.sh "v22.1.5"
+./teamcity-build/build-teamcity.sh "v22.1.12"
--- teamcity-build/build-teamcity-22.2.sh
+++ teamcity-build/build-teamcity-22.2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 
-./teamcity-build/build-teamcity.sh "v22.2.0"
+./teamcity-build/build-teamcity.sh "v22.2.1"
